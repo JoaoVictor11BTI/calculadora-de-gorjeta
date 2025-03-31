94b84de979ac10f24dc7789c48f57ebaae52737e
joaovictor.tsouzaz <joaovictor.tsouzaz@gmail.com>
2025-03-31 03:16:12 +0000

    Ajustes do hover do botao tip e do inner spin button

--- main.js
+++ main.js
@@ -40,11 +40,8 @@ function receiveCustomTipPercentage() {
     let customTipInput = document.querySelector("#custom-tip")
     tipPercentage = customTipInput.valueAsNumber / 100
 
-    if (buttonSelected !== null) {
         buttonSelected.classList.remove("button-selected");
         buttonSelected = null;
-    }
-    calculate();
 }
 
 function calculate(){
@@ -63,4 +60,8 @@ function calculate(){
     }else{
         console.log("ainda não é possivel calcular")
     }
+}
+ 
+function reset(){
+    
 }
